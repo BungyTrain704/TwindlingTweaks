189aae566289962ec171f5ba889fb492d1ab8e98
BungyTrain704 <82040656+BungyTrain704@users.noreply.github.com>
2023-07-21 15:37:44 -0400

    Delete AlluminiteBlock.java

--- java/bungy/twindling/tweaks/main/AlluminiteBlock.java
+++
@@ -1,9 +0,0 @@
-package bungy.twindling.tweaks.main;
-
-import net.minecraft.block.Block;
-
-public class AlluminiteBlock extends Block {
-    public AlluminiteBlock(Settings settings) {
-        super(settings);
-    }
-}
